ec01ef7032b9be3485e3dfe1b9b77d762a26bdd4
DominikSzkotland <dszlencka@outlook.com>
2023-10-08 16:32:06 +0200

    Zmiana na offline na specjalne życzenie
    Dzięki przeniesieniu tablicy z zewnętrznego pliku strona jest offline. (script nie jest modularny) Zmiana nastąpiła również w algorytmie ponieważ na specjalne życzenie z ukłądu 5x3 zmieniłem ukłąd na 2x2.

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
-import tablica_kategorii_i_piosenek from './tablica_kategorii_i_piosenek.js';
+// Tablica z piosenkami
+
+const tablica_kategorii_i_piosenek = [
+    {
+      Kategoria: "Powiew lata",
+      Piosenka: "Wakacje znów będą wakacje"
+    },
+    {
+      Kategoria: "Zazwyczaj bez łez",
+      Piosenka: "Chłopaki nie płaczą"
+    },
+    {
+      Kategoria: "Morskie opowieść",
+      Piosenka: "Morskie opowieść"
+    },
+    {
+      Kategoria: "Bojowa szanta",
+      Piosenka: "Bitwa (szanta)"
+    },
+    {
+      Kategoria: "Odkrywcy",
+      Piosenka: "Drobnostka z Vaiany"
+    },
+    {
+      Kategoria: "Dla każdego",
+      Piosenka: "We are the champion"
+    },
+    {
+      Kategoria: "Nie dla Kubusia Puchatka",
+      Piosenka: "Pszczółka Maja"
+    },
+    {
+      Kategoria: "Kawa czy herbata",
+      Piosenka: "Kawka na wynos"
+    },
+    {
+      Kategoria: "Strata",
+      Piosenka: "O Ela straciłaś przyjaciela"
+    },
+    {
+      Kategoria: "Prestiż w górach",
+      Piosenka: "Zakopane"
+    },
+    {
+      Kategoria: "World of tanks",
+      Piosenka: "4 pancerni i pies"
+    },
+    {
+      Kategoria: "Działania na liczbach ujemnych",
+      Piosenka: "Mniej niż 0"
+    },
+    {
+      Kategoria: "Marzenia zawodowe",
+      Piosenka: "Chciałbym być marynarzem"
+    },
+    {
+      Kategoria: "Dla murarza",
+      Piosenka: "Czerwony jak cegła"
+    },
+    {
+      Kategoria: "Nic nie muszę, wszystko mogę",
+      Piosenka: "Mam tę moc"
+    },
+    {
+      Kategoria: "Zimowe szaleństwo",
+      Piosenka: "Ulepimy dziś bałwana"
+    },
+    {
+      Kategoria: "Warzywna piosenka",
+      Piosenka: "Ogórek zielony ma garniturek"
+    },
+    {
+      Kategoria: "Kolejowa piosenka",
+      Piosenka: "Nic nie robić nie mieć zmartwień"
+    },
+    {
+      Kategoria: "Skok",
+      Piosenka: "Gumisie"
+    },
+    {
+      Kategoria: "Zero trosk",
+      Piosenka: "Hakuna matata"
+    }
+  ];
+
+// Dalsza czesc
 
 const startButton = document.getElementById("startButton");
 const startBox = document.getElementById("startBox");
@@ -16,44 +101,27 @@ function start()
         const box = document.createElement('div');
         box.textContent = tablica_kategorii_i_piosenek[i].Kategoria;
         box.setAttribute("data-title",tablica_kategorii_i_piosenek[i].Piosenka)
-        if(i%3 == 1)
+        
+        if(i%4 == 0)
         {
-            box.classList.add("rzadPierwszy");
-        }
-        if(i%3 == 2)
-        {
+            box.classList.add("kolumna2")
             box.classList.add("rzadDrugi");
+            
         }
-        if(i%3 == 0)
-        {
-            box.classList.add("rzadTrzeci");
-        }
-        if(i==0)
+        if(i%4 == 1)
         {
             box.classList.add("kolumna1")
+            box.classList.add("rzadPierwszy");
         }
-        else
+        if(i%4 == 2)
         {
-            if(i%5 == 0)
-            {
-                box.classList.add("kolumna5")
-            }
-            if(i%5 == 1)
-            {
-                box.classList.add("kolumna1")
-            }
-            if(i%5 == 2)
-            {
-                box.classList.add("kolumna2")
-            }
-            if(i%5 == 3)
-            {
-                box.classList.add("kolumna3")
-            }
-            if(i%5 == 4)
-            {
-                box.classList.add("kolumna4")
-            }
+            box.classList.add("kolumna2")
+            box.classList.add("rzadPierwszy");
+        }
+        if(i%4 == 3)
+        {
+            box.classList.add("kolumna1")
+            box.classList.add("rzadDrugi");
         }
         divGra.appendChild(box);
     }
@@ -81,3 +149,4 @@ function ClickStart()
     start()
     startBox.classList.add("hideForever")
 }
+
